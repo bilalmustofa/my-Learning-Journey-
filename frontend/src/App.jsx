843ccbd2cca8428cad967ee62f2e6351aed92f98
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-22 09:59:34 +0300

    feat: integrate Tailwind CSS and add Home and Header components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Home from './pages/home/Home.jsx'
 
 function App() {
 
   return (
     <>
+    <Home />
     </>
   )
 }
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header.jsx"
+
+function Home() {
+  return (
+    <>
+        <Header />
+    </>
+  )
+}
+
+export default Home
