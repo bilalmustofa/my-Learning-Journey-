ab1f746006b2d8c62c8f41ce49ffcfe91af976e0
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-22 12:23:45 +0300

    feat: add logo and enhance Header component with navigation and icons

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,69 @@
+import { Link } from 'react-router-dom'
+import logo from "../../assets/images/logo.png";
+import { CiSearch } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { FiUser } from "react-icons/fi";
+import { IoIosArrowDown } from "react-icons/io";
 
 function Header() {
   return (
     <>
+      <header className="bg-transparent fixed top-0 left-0 w-full z-1000 py-5 sm:py-7 lg:py-10">
+         <div className="flex items-center justify-between max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-10 xl:px-14">
+
+          {/* Left Side */}
+          <div className="flex items-center gap-6 lg:gap-8 shrink-0">
+            {/* Logo */}
+            <div>
+              <img className="w-20 sm:w-24 lg:w-25 h-auto cursor-pointer hover:opacity-80" src={logo} alt="Logo" />
+            </div>
+
+            {/* Navigation */}
+            <nav className="hidden lg:flex items-center gap-4 xl:gap-5 text-[14px] font-medium">
+              <Link className="hover:text-[#b3b3b0]" href="">Home</Link>
+              <Link className="hover:text-[#b3b3b0]" href="">TvShows</Link>
+              <Link className="hover:text-[#b3b3b0]" href="">Movies</Link>
+              <Link className="hover:text-[#b3b3b0]" href="">New & Popular</Link>
+              <Link className="hover:text-[#b3b3b0]" href="">My List</Link>
+              <Link className="hover:text-[#b3b3b0]" href="">Browse by Language</Link>
+            </nav>
+          </div>
+
+          {/* Right Side */}
+          <div className="flex items-center gap-4 sm:gap-5 shrink-0">
+            {/* search */}
+            <div className="relative flex items-center">
+              <button>
+                <CiSearch size={20} className="cursor-pointer" />
+              </button>
+              <input type="text" placeholder="Search" className="hidden sm:block ml-2 w-24 md:w-32 lg:w-40 bg-transparent outline-none text-sm" />
+            </div>
+
+            {/* notification */}
+            <div className="relative flex items-center cursor-pointer">
+              {/* notification icon */}
+              <button>
+                <IoMdNotificationsOutline size={20} className="cursor-pointer"/>
+              </button>
+              {/* notification number */}
+              <span className="absolute -top-2 -right-2 text-[10px]">2</span>
+            </div>
+
+            {/* profile */}
+            <div>
+              <button className="flex items-center gap-1">
+                {/* user icon */}
+                <FiUser size={20} className="cursor-pointer"/>
+                {/* dropDown Icon */}
+                <IoIosArrowDown className="hidden sm:block cursor-pointer"/>
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
