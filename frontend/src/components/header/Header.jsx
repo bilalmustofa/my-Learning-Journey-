3f993e8af9b769ecee0fcfe7dd76476b3e63ce1d
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-23 12:04:07 +0300

    feat: create Hero component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
   return (
     <>
-      <header className="bg-transparent fixed top-0 left-0 w-full z-1000 py-5 sm:py-7 lg:py-10">
+      <header className="fixed top-0 left-0 w-full z-1000 py-5 sm:py-7 lg:py-8 bg-black text-white">
          <div className="flex items-center justify-between max-w-[1920px] mx-auto px-4 sm:px-6 lg:px-10 xl:px-14">
 
           {/* Left Side */}
@@ -41,7 +41,7 @@ function Header() {
               <button onClick={() => setSearchOpen(!searchOpen)}>
                 <CiSearch size={20} className="cursor-pointer" />
               </button>
-              <input type="text" placeholder="Search" className={`bg-transparent outline-none text-sm transition-all duration-300 ${searchOpen ? "w-32 sm:w-40 ml-2" : "w-0 ml-0" }`} />
+              <input type="text" placeholder="Search" className={`bg-transparent outline-none text-sm transition-all duration-300 py-1 ${searchOpen ? "w-32 sm:w-40 ml-2" : "w-0 ml-0" }`} />
             </div>
 
             {/* notification */}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header/Header.jsx"
+import Hero from "../../components/hero/Hero.jsx"
 
 function Home() {
   return (
     <>
         <Header />
+        <Hero />
     </>
   )
 }
