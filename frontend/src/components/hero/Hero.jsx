95ba0ec8f3c56a6d720da0c85c9ddbc519ceed62
bilalmustofa <bilalmustofa146@gmail.com>
2026-08-24 22:49:00 +0300

    feat: create Movies Card component

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -13,8 +13,8 @@ function Hero() {
         <div className="absolute inset-0 bg-black/40"></div>
 
         {/* Hero content */}
-        <div className="relative z-10 flex min-h-screen items-center">
-          <div className="max-w-2xl px-6 md:px-12 lg:px-16">
+        <div className="relative z-10 flex min-h-screen items-center pt-40">
+          <div className="max-w-2xl px-6 md:px-12 lg:px-10">
             {/* Logo */}
             <img src={logo} alt="Netflix logo" className="w-32 md:w-40 mb-4" />
 
@@ -32,13 +32,13 @@ function Hero() {
 
             {/* Buttons */}
             <div className="flex gap-4">
-              <button className="flex items-center gap-2 rounded-md bg-white px-6 py-2 font-semibold text-black hover:bg-gray-400 transition">
-                <FiPlay />
+              <button className="flex items-center gap-2 rounded-md bg-white px-6 py-2 font-semibold hover:bg-gray-400 transition">
+                <FiPlay size={20}/>
                 Play
               </button>
 
               <button className="flex items-center gap-2 rounded-md bg-white px-6 py-2 font-semibold hover:bg-gray-300 transition">
-                <FiInfo />
+                <FiInfo size={20}/>
                 My List
               </button>
             </div>
