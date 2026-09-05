cf395b33fea79913a3460890cc48b95e10da94ab
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-05 11:09:40 +0300

    feat: add validation for question input in createConservationService function

--- AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
+++ AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
@@ -1,6 +1,14 @@
 
 export async function createConservationService(question) {
     try {
+
+        // Validation
+        if(!question.trim()){
+            const error = new Error(' Question is required');
+            error.status = 400;
+            throw error;
+        }
+
         return `chat saved to db with question: ${question}`;
     } catch (error) {
         throw error;
