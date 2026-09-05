710524c3392a0b11f6964d71e3597e633cb8141e
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-05 11:08:12 +0300

    feat: implement createConservationService function for chat service

--- AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
+++ AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
@@ -1,1 +1,8 @@
 
+export async function createConservationService(question) {
+    try {
+        return `chat saved to db with question: ${question}`;
+    } catch (error) {
+        throw error;
+    }
+}
