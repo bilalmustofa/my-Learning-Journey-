0e1f211a9c8dda26ed03b8a973b3440ac6d591e0
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-05 11:08:33 +0300

    feat: add error handler middleware for centralized error management

--- AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/middleware/error-handler.js
+++ AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/middleware/error-handler.js
@@ -1,1 +1,8 @@
 
+export const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+    return res.status(err.status || 500).json({
+        status: false,
+        message: err.message || 'Something want wrong try again later'
+    })
+}
