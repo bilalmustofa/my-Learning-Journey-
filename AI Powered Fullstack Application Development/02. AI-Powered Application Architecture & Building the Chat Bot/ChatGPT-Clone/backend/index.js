fe54b0e0869a4930738d8cc6f87684c124417f48
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-05 10:25:43 +0300

    feat: set up express server with main router and error handler

--- AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/index.js
+++ AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/index.js
@@ -1,9 +1,18 @@
 import 'dotenv/config'
 import express from "express";
 import dbConfig from './db/db.config.js'
+import mainRouter from './src/api/main.routes.js';
+import { errorHandler } from './src/middleware/error-handler.js'
 
 const app = express();
 
+app.use(express.json());
+app.use('/api', mainRouter);
+
+
+app.use(errorHandler);
+
+// server starter function
 async function serverStart() {
   try {
 
@@ -17,9 +26,10 @@ async function serverStart() {
         }
       console.log("server running on port 3000");
     });
+
   } catch (error) {
     console.log(error.message)
   }
 }
 
-serverStart()
+serverStart();
