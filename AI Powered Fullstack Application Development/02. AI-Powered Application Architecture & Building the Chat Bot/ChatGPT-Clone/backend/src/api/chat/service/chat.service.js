cb29a27757f797d085141464bff905fa25629346
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-06 11:34:11 +0300

    feat: implement generateAssistantAnswer function for chat service integration

--- AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
+++ AI Powered Fullstack Application Development/02. AI-Powered Application Architecture & Building the Chat Bot/ChatGPT-Clone/backend/src/api/chat/service/chat.service.js
@@ -1,4 +1,5 @@
 import dbConfig from "../../../../db/db.config.js";
+import { GoogleGenAI } from '@google/genai';
 
 // get recent conversation row from db
 const getRecentConversationRows = async (limit = 5) => {
@@ -13,6 +14,35 @@ const getRecentConversationRows = async (limit = 5) => {
     );
     return rows.reverse();
 }
+
+const GEMINI_MODEL = process.env.GEMINI_MODEL || "gemini-3.6-flash";
+const geminiClient = new GoogleGenAI({ apiKey:process.env.GEMINI_API_KEY });
+
+const generateAssistantAnswer = async({historyRows, question})=>{
+
+  const formattedHistory = historyRows.map(row=>({
+    role: row.role === 'assistant' ? 'model'  :'user',
+    parts: [{ text:row.content }]
+  }));
+
+  const chat = geminiClient.chats.create({
+    model : GEMINI_MODEL,
+    config: {
+            maxOutputTokens: 1024,
+        },
+    history : formattedHistory
+  });
+
+  const result = await chat.sendMessage({
+    message: question,
+  });
+
+   return {
+    text: result.text,
+    totalTokens: result.usageMetadata.totalTokenCount
+   }
+}
+
 
 export async function createConservationService(question) {
     try {
@@ -31,7 +61,6 @@ export async function createConservationService(question) {
         const query = `INSERT INTO conversations (content, role) VALUES (?, 'user')`
         const [result] = await dbConfig.query(query, [question])
 
-        return historyRows;
         
     } catch (error) {
         throw error;
