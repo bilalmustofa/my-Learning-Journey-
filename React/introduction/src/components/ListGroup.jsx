b92d7d8b613024cc0f07c7b0b87031a1d159a1dc
bilalmustofa <bilalmustofa146@gmail.com>
2026-09-25 23:25:42 +0300

    feat: update ListGroup component with cities and fruits lists dynamically  by array map method

--- React/introduction/src/components/ListGroup.jsx
+++ React/introduction/src/components/ListGroup.jsx
@@ -1,21 +1,25 @@
+
 function ListGroup() {
+
+    const cites = ['Addis Ababa', 'Wollo', 'Adama', 'Wolkite', 'Baher Dar'];
+    const fruits =['Banana', 'Apple', 'Avocado', 'Orange'];
+    
   return (
     <>
-    <h1>List</h1>
+    <h1>List of items</h1>
       <div>
         <ul>
-          <li>Apple</li>
-          <li>Ball</li>
-          <li>Car</li>
-          <li>Dog</li>
-          <li>Egg</li>
+            <h3>Cites List</h3>
+          {cites.map((city, i) => (
+            <li key={i}>{city}</li>
+          ))}
         </ul>
+
         <ol>
-          <li>Ant</li>
-          <li>Banana</li>
-          <li>Cow</li>
-          <li>Doll</li>
-          <li>Ele</li>
+          <h3>Fruits List</h3>
+          {fruits.map((fruit, i) => (
+            <li key={i}>{fruit}</li>
+          ))}
         </ol>
       </div>
     </>
